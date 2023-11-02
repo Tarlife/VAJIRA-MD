6fc6435a2ff7cade0b640a1da13f13a6d963a84a
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-11-03 04:43:01 +0530

    😈 ＶＡＪＩＲＡ - ＭＤ 😈

--- plugins/group.js
+++ plugins/group.js
@@ -17,33 +17,3 @@ command(
   }
 );
 
-async function updateProfilePicture(jid, imag, message) {
-  const { query } = message.client;
-  const { img } = await generateProfilePicture(imag);
-  await query({
-    tag: "iq",
-    attrs: {
-      to: jid,
-      type: "set",
-      xmlns: "w:profile:picture",
-    },
-    content: [
-      {
-        tag: "picture",
-        attrs: { type: "image" },
-        content: img,
-      },
-    ],
-  });
-}
-
-async function generateProfilePicture(buffer) {
-  const jimp = await Jimp.read(buffer);
-  const min = jimp.getWidth();
-  const max = jimp.getHeight();
-  const cropped = jimp.crop(0, 0, min, max);
-  return {
-    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
-    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
-  };
-}
